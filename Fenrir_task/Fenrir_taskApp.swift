0dee0598935290ef977661a9d5a789c40631a69c
haruna1256 <haru_kawagishi@icloud.com>
2025-06-04 21:27:43 +0900

    Initial Commit

---
+++ Fenrir_task/Fenrir_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fenrir_taskApp.swift
+//  Fenrir_task
+//
+//  Created by 川岸遥奈 on 2025/06/04.
+//
+
+import SwiftUI
+
+@main
+struct Fenrir_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
