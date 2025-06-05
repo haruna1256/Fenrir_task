2166f5410e337f653e9c47b4d27b458bf413e520
haruna1256 <haru_kawagishi@icloud.com>
2025-06-06 02:06:12 +0900

    [add] スマホに表示される画面の入力処理　コメントはまだ項目を書いていないためエラーが起きるから

--- Fenrir_task/ContentView.swift
+++ Fenrir_task/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    //    入力されたものを受け取る変数
+    @State private var inputText: String = ""
+    //    判定結果の変数
+    @State private var result: Bool = false
     var body: some View {
         VStack {
-//            入力画面と表示画面を作る
+            Text("括弧閉じてるか判定")
+                .font(.title)
+                .bold()
+            //            入力項目
+            TextField("例：(){}[]", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            //            判定処理を促すボタン
+            Button("判定する") {
+//                result = isValid(inputText)
+            }
+            .padding()
+            .background(Color.red)
+            .foregroundStyle(Color.white)
+            .cornerRadius(10)
+            
+//            if let resultText = result {
+//                Text(resultText ? "正しい" : "正しくない")
+//                    .font(.headline)
+//                    
+//                
+//            }
+            Spacer()
+            
         }
         .padding()
     }
@@ -18,9 +45,12 @@ struct ContentView: View {
 
 
 //　かっこが閉じているか判定するメソッド
-//func isValid(_s: String) -> Bool {‬
-//
-//‭ }‬
+//func isValid(_ s: String) -> Bool {‬
+//    
+//    
+//    
+//    return true
+//    ‭ }‬
 
 
 
